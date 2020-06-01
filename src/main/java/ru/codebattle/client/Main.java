e5ee17f572f4cd250cdb233283b47d0df091f722
Gibelda <36190202+Gibelda@users.noreply.github.com>
2020-06-01 22:56:07 +0300

    Update Main.java

--- src/main/java/ru/codebattle/client/Main.java
+++ src/main/java/ru/codebattle/client/Main.java
@@ -8,15 +8,14 @@ import ru.codebattle.client.api.*;
 
 public class Main {
 
-    private static final String SERVER_ADDRESS = "http://codebattle-pro-2020s1.westeurope.cloudapp.azure.com/codenjoy-contest/board/player/d7rl93d0nx24x6zlv5h0?code=8926581544870510245&gameName=snakebattle";
-
+    private static final String SERVER_ADDRESS = "http://codebattle-pro-2020s1.westeurope.cloudapp.azure.com/codenjoy-contest/board/player/iu6fs8g2wv2laakcppdz?code=3055726951586902261&gameName=snakebattle";
     public static void main(String[] args) throws URISyntaxException, IOException {
+        long date = new Date().getTime();
         SnakeBattleClient client = new SnakeBattleClient(SERVER_ADDRESS);
         client.run(gameBoard -> {
-            long date = new Date().getTime();
             Direction direction = Strategy.chooseRightDirection(gameBoard);
-            //System.out.println(new Date().getTime() - date);
-            boolean act = Strategy.chooseAct();
+            System.out.println(new Date().getTime() - date);
+            boolean act = Strategy.chooseAct(gameBoard);
             return new SnakeAction(act, direction);
         });
         System.in.read();
